c185cfe543dbd30f835df6f8e25cfca66757359f
hanzala <hanzalaazafar631@gmail.com>
2024-11-23 15:31:34 +0500

    Updated code to use next/image for image optimization

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react"
 
 const About = () => {
   return (
     <div>
-      <section className="about" style={{backgroundImage:"url(b2.jpg)"}}>
+      <section className="about" style={{backgroundImage: "url(b2.jpg)"}}>
         <div className="about-content">
-          <h2 className="fade-in">About - Us <br />Your Ultimate Sunglasses/Optical Store Welcome to  <b> Eyes more</b> </h2>
-          <p> Your Premier Online Sunglasses/Optical  Store we are more than just an online store; we are your trusted partner in finding the perfect eyewear to complement your style and protect your eyes. Our passion for optics and fashion drives us to curate a diverse range of sunglasses that embody the latest trends while ensuring top-notch quality and comfort.
-
-A Vision of Excellence in Eyewear
-As a dedicated optics store, we understand the significance of sunglasses beyond being a mere accessories. Sunglasses are essential for safeguarding your eyes from harmful UV rays while enhancing your look. That's why we are committed to offering a thoughtfully curated collection that not only elevates your style but also provides optimal eye protection.</p>
+          <h2 className="fade-in">About - Us <br />Your Ultimate Sunglasses/Optical Store Welcome to <b> Eyes more</b></h2>
+          <p>Your Premier Online Sunglasses/Optical Store. We are more than just an online store; we are your trusted partner in finding the perfect eyewear to complement your style and protect your eyes. Our passion for optics and fashion drives us to curate a diverse range of sunglasses that embody the latest trends while ensuring top-notch quality and comfort.
+          A Vision of Excellence in Eyewear. As a dedicated optics store, we understand the significance of sunglasses beyond being a mere accessories. Sunglasses are essential for safeguarding your eyes from harmful UV rays while enhancing your look. That&apos;s why we are committed to offering a thoughtfully curated collection that not only elevates your style but also provides optimal eye protection.</p>
         </div>
       </section>
     </div>
--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -1,27 +1,34 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Collections = () => {
 
-  const glassesData =[
-    {id: 1, name: 'Rayban', price: 2200 , description: 'Rayban-Sunglass  Aviator 1st Copy' , image:'/item5.jpg'},
-    {id: 2, name: 'Rayban', price: 3200 , description: 'Rayban-Sunglass  Wafarer 1st Copy' , image:'/item6.jpg'},
-    {id: 3, name: 'Metal-sunglass', price: 2100 , description: 'Metal Sunglass Sleek Design' , image:'/m1.jpg'},
-    {id: 4, name: 'Rayban', price: 1900 , description: 'Rayban-Sunglass  Hexagon 1st Copy' , image:'/item7.jpg'},
-    {id: 5, name: 'Oakley-Frame', price: 3200 , description: 'Oakley-Frame 1st Copy' , image:'/m2.jpg'},
-    {id: 6, name: 'Transparent-Frame', price: 1500 , description: 'Tr-material frame Uni-sex model' , image:'/item4.jpg'},
-    {id: 7, name: 'Sqare-frame', price: 2000 , description: 'Square-sheet Frame for gents' , image:'item1.jpg'},
-    {id: 8, name: 'Fashion-frame', price: 1400 , description: 'Metal-square frame for ladies' , image:'item2.jpg'}
+  const glassesData = [
+    {id: 1, name: "Rayban", price: 2200 , description: 'Rayban-Sunglass  Aviator 1st Copy' , image:'/item5.jpg'},
+    {id: 2, name: "Rayba", price: 3200 , description: 'Rayban-Sunglass  Wafarer 1st Copy' , image:'/item6.jpg'},
+    {id: 3, name: "Metal-sunglass", price: 2100 , description: 'Metal Sunglass Sleek Design' , image:'/m1.jpg'},
+    {id: 4, name: "Rayban", price: 1900 , description: 'Rayban-Sunglass  Hexagon 1st Copy' , image:'/item7.jpg'},
+    {id: 5, name: "Oakley-Frame", price: 3200 , description: 'Oakley-Frame 1st Copy' , image:'/m2.jpg'},
+    {id: 6, name: "Transparent-Frame", price: 1500 , description: 'Tr-material frame Uni-sex model' , image:'/item4.jpg'},
+    {id: 7, name: "Sqare-frame", price: 2000 , description: 'Square-sheet Frame for gents' , image:'/item1.jpg'},
+    {id: 8, name: "Fashion-frame", price: 1400 , description: 'Metal-square frame for ladies' , image:'/item2.jpg'}
   ]
+
   return (
     <div>
       <div className="collection-glasses">
         {glassesData.map((glasses) => (
-          <div key ={glasses.id} className="collection-card">
-           <img src={glasses.image} alt={glasses.name} />
-           <h3>{glasses.name}</h3>
-           <p>{glasses.description}</p>
-           <div className="price">RS={glasses.price}/-</div>
-           <button>Add to Cart</button>
+          <div key={glasses.id} className="collection-card">
+            <Image 
+              src={glasses.image} 
+              alt={glasses.name} 
+              width={400} 
+              height={500} // Set appropriate width and height
+            />
+            <h3>{glasses.name}</h3>
+            <p>{glasses.description}</p>
+            <div className="price">RS={glasses.price}/-</div>
+            <button>Add to Cart</button>
           </div>
         ))}
       </div>
